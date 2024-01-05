34f77211e02c3cd043c481dee51d37e95d783def
je <je@h>
2024-01-06 00:53:27 +0800

    ..

--- wireguard_keepalive.py
+++ wireguard_keepalive.py
@@ -32,9 +32,9 @@ log.setLevel(logging.INFO)
 log.info("Wireguard Keepalive Running")
 
 
-def check_network():
+def check_network(ip):
     try:
-        subprocess.run(["ping", "-c", "1", "10.1.1.1"], check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        subprocess.run(["ping", "-c", "1", ip], check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         return True
     except subprocess.CalledProcessError as e:
         # 获取 ping 命令的输出
@@ -46,41 +46,59 @@ def check_network():
             log.info(f"Ping error: {output}")
 
 
-def restart_wireguard():
+def restart_wireguard(wg_name):
     # 重新连接 WireGuard（请替换为你的命令）
-    subprocess.run(["systemctl", "restart", "wg-quick@wg0"], check=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    subprocess.run(["systemctl", "restart", f"wg-quick@{wg_name}"], check=True, stdout=subprocess.DEVNULL,
+                   stderr=subprocess.DEVNULL)
     log.info("WireGuard reconnected.")
 
 
-# 允许ping失败的次数
-allow_max_error_count = 3
-down_count = 0
-# 上次是在线的
-is_last_up = False
+class WgConfig:
+    # 允许ping失败的次数
+    allow_max_error_count = 3
+
+    def __init__(self, wg_name, ip):
+        self.wg_name = wg_name
+        self.ip = ip
+        self.down_count = 0
+        # 上次是在线的
+        self.is_last_up = False
+
+
+def process_one(wg_inst):
+    if check_network(wg_inst.ip):
+        wg_inst.down_count = 0
+
+        if wg_inst.is_last_up:
+            pass
+        else:
+            log.info(f"Network {wg_inst.wg_name} is up.")
+
+        wg_inst.is_last_up = True
+    else:
+        wg_inst.down_count += 1
+        if wg_inst.down_count == 1:
+            log.info(f"Network {wg_inst.wg_name} is down.")
+        else:
+            pass
+
+        log.info(f"Network {wg_inst.wg_name} Waiting {wg_inst.down_count}")
+
+        if wg_inst.down_count > WgConfig.allow_max_error_count:
+            log.info(f"Network {wg_inst.wg_name} Ping failed {wg_inst.down_count} times. Reconnecting WireGuard...")
+            restart_wireguard(wg_inst.wg_name)
+
+        wg_inst.is_last_up = False
+
+
+wg_config_list = [
+    WgConfig("wg0", "10.1.1.1"),
+    WgConfig("wg1", "10.4.4.1")
+]
 
 while True:
-    if check_network():
-        down_count = 0
-        if is_last_up:
-            pass
-        else:
-            log.info("Network is up.")
-
-        is_last_up = True
-    else:
-        down_count += 1
-        if down_count == 1:
-            log.info("Network is down.")
-        else:
-            pass
-
-        log.info(f"Waiting {down_count}")
-
-        if down_count > allow_max_error_count:
-            log.info(f"Ping failed {down_count} times. Reconnecting WireGuard...")
-            restart_wireguard()
-
-        is_last_up = False
+    for wg in wg_config_list:
+        process_one(wg)
 
     # 每隔一段时间检测一次网络状态（秒）
     time.sleep(10)
