ee86cfca07b80d21e0d9fc4f2bcdcb6af90d893f
je <je@jueane.com>
2024-01-09 16:46:48 +0800

    ..

--- install.py
+++ install.py
@@ -18,12 +18,18 @@ try:
     os.symlink(source_file_py, target_link_py)
     print(f"软链接成功创建：{target_link_py}")
 
+except FileExistsError:
+    print(f"{target_link_py}软链接已存在，无需创建。")
+except Exception as e:
+    print(f"创建软链接时发生错误：{e}")
+
+# 创建软链接
+try:
     # 创建服务文件软链接
     os.symlink(source_file_service, target_link_service)
     print(f"软链接成功创建：{target_link_service}")
 
 except FileExistsError:
-    print("软链接已存在，无需创建。")
+    print(f"{target_link_service}软链接已存在，无需创建。")
 except Exception as e:
     print(f"创建软链接时发生错误：{e}")
-
